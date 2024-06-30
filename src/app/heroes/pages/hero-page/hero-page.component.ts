9f79251ad4d701f0ef5e1e1fb74b4bc176269910
Marco Parra <marcoparra311@gmail.com>
2024-06-30 07:07:54 -0400

    Create hero page taking id from url using params features to difference each hero

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { HeroElement } from '../../interfaces/hero.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap, delay } from 'rxjs';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+  hero?: HeroElement;
 
+  constructor(
+    private heroeService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap((params) => this.heroeService.getHeroById(params['id'])))
+      .subscribe((hero) => {
+        if (!hero) this.router.navigate(['/heroes/list']);
+        this.hero = hero;
+      });
+  }
+
+  goBack() {
+    this.router.navigate(['heroes/list']);
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environments';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { HeroElement } from '../interfaces/hero.interface';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
   private baseUrl: string = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getHeroes():Observable<HeroElement[]> {
+  getHeroes(): Observable<HeroElement[]> {
     return this.http.get<HeroElement[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroById(id: string): Observable<HeroElement | undefined> {
+    return this.http
+      .get<HeroElement>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(catchError((error) => of(undefined)));
   }
 }
